029e73571bd147454947c700f64a7664767ab4e4
Your Name <you@example.com>
2024-01-21 13:47:33 +0200

    get firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import { burger } from './modules/burger';
-import { firebase } from './modules/firebase';
+import firebase from './modules/firebase';
+import { get, ref, child } from 'firebase/database';
+
+const DBref = ref(firebase.getDatabase());
+
+get(child(DBref, `main/Biography/img`))
+.then((snap) => {
+    document.querySelector('#biography__image__img').src = snap.val();
+})
+.catch((e) => {
+    document.querySelector('#biography__image__img').src = '/defualt.img'
+})
+
+
 
