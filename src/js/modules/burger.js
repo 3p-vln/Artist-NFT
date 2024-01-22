c0d6351f187d3081bf35be5f832c21b0883f0650
Your Name <you@example.com>
2024-01-22 19:02:45 +0200

    fix burger, adaptive main-biography

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -3,22 +3,21 @@ function burger() {
     const headerMenu = document.querySelector('.header__menu');
     const body = document.querySelector("body");
     const burgerBackdrop = document.querySelector('.burger-backdrop');
-    const buttonauthorization = document.querySelector('.authorization');
+    const buttonAuthorization = document.querySelector('.authorization');
 
     if (headerBurger) {
         headerBurger.addEventListener('click', () => {
             headerBurger.classList.toggle("_active");
             burgerBackdrop.classList.toggle('_active');
             headerMenu.classList.toggle('_active');
-            buttonauthorization.classList.toggle('_active');
+            buttonAuthorization.classList.toggle('_active');
             body.classList.toggle("_lock");
         });
-        
         burgerBackdrop.addEventListener('click', () => {
             headerBurger.classList.toggle("_active");
             burgerBackdrop.classList.toggle("_active");
             headerMenu.classList.toggle('_active');
-            buttonauthorization.classList.toggle('_active');
+            buttonAuthorization.classList.toggle('_active');
             body.classList.toggle("_lock");
         });
     }
