596ed075423a3d728119b768633f824ec21d76f3
3p-vln <vldknovak@gmail.com>
2024-01-24 21:04:35 +0200

    fix adapt custom

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,29 +1,39 @@
 import $ from 'jquery';
-import {db} from './modules/firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import { db } from './modules/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { customLoad } from './modules/customLoad';
 
 async function snap() {
-    const querySnapshot = await getDocs(collection(db, "art"));
-    querySnapshot.forEach((doc) => {
-        new customLoad(doc.data(), doc.id);
-    });
+  const querySnapshot = await getDocs(collection(db, "art"));
+  querySnapshot.forEach((doc) => {
+    new customLoad(doc.data(), doc.id);
+  });
 }
 snap();
 
+async function snap2() {
+  const querySnapshot = await getDocs(collection(db, "custom"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    document.querySelector('#clother-webp').srcset = data.imgWebP;
+    document.querySelector('#clother-img').src = data.img;
+  });
+}
+snap2();
+
 function focus() {
-    $('.custom-mesh').on('click', '.card', (event) => {
-        $('.active').removeClass('active');
-        const clickedElement = $(event.currentTarget);
-        clickedElement.addClass('active');
-        clickedElement.find('.btn').removeClass('btn_active');
-        setTimeout(function(){
-          clickedElement.find('.btn').addClass('btn_active');
-        },1);
-      });
-      
-      $('.custom-mesh').on('mouseleave', '.card', () => {
-        $('.active').removeClass('active');
-      });
+  $('.custom-mesh').on('click', '.card', (event) => {
+    $('.active').removeClass('active');
+    const clickedElement = $(event.currentTarget);
+    clickedElement.addClass('active');
+    clickedElement.find('.btn').removeClass('btn_active');
+    setTimeout(function () {
+      clickedElement.find('.btn').addClass('btn_active');
+    }, 1);
+  });
+
+  $('.custom-mesh').on('mouseleave', '.card', () => {
+    $('.active').removeClass('active');
+  });
 }
 focus();
