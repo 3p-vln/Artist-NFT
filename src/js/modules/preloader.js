e7efc776309914fbea7fd095df0c30b88032e83f
3p-vln <vldknovak@gmail.com>
2024-01-27 14:27:15 +0200

    fix grid

--- src/js/modules/preloader.js
+++ src/js/modules/preloader.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 function preloader() {
     $(window).on('load', function () {
-        $('.preloader').delay(500).fadeOut(500);
+        $('.preloader').delay(1000).fadeOut(500);
     });
 }
 
