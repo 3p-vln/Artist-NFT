3180c5ebd1589438990ece771173b58c16edba8a
3p-vln <vldknovak@gmail.com>
2024-01-31 14:39:27 +0200

    become a part with adapt

--- src/js/oneArt.js
+++ src/js/oneArt.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import { db } from './modules/firebase';
 import { collection, getDocs } from 'firebase/firestore';
+import { validate } from './main/contactValidate';
 
 async function getArts() {
     const urlParams = new URLSearchParams(window.location.search);
