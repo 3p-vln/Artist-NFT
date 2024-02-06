c25360eb5dd92b427521e626eb10423b095d5b2f
3p-vln <vldknovak@gmail.com>
2024-02-06 16:30:39 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,50 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const paths = {
-  src: path.resolve(__dirname, 'src'),
-  build: path.resolve(__dirname, 'dist'),
+    src: path.resolve(__dirname, 'src'),
+    build: path.resolve(__dirname, 'dist'),
 };
 
-export const webpackConfig = (isMode) => {
-  return {
-    entry: {
-      main: path.join(paths.src, 'js/main.js'),
-      art: path.join(paths.src, 'js/art.js'),  
-      custom: path.join(paths.src, 'js/custom.js'),  
-      oneArt: path.join(paths.src, 'js/oneArt.js'),  
-      become: path.join(paths.src, 'js/become.js'),  
-      pay: path.join(paths.src, 'js/pay.js'),  
-      authorization: path.join(paths.src, 'js/authorization.js'),  
-      app: path.join(paths.src, 'js/app.js'),    
-    },
-
-    mode: isMode ? 'development' : 'production',
-
-    output: {
-      path: path.join(paths.build, 'js'),
-      filename: '[name].min.js',
-      publicPath: '/',
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-
-          resolve: {
-            fullySpecified: false,
-          },
+export const webpackConfig = isMode => {
+    return {
+        entry: {
+            main: path.join(paths.src, 'js/main.js'),
+            art: path.join(paths.src, 'js/art.js'),
+            custom: path.join(paths.src, 'js/custom.js'),
+            oneArt: path.join(paths.src, 'js/oneArt.js'),
+            become: path.join(paths.src, 'js/become.js'),
+            pay: path.join(paths.src, 'js/pay.js'),
+            authorization: path.join(paths.src, 'js/authorization.js'),
+            app: path.join(paths.src, 'js/app.js'),
         },
-      ],
-    },
-    devtool: isMode ? 'source-map' : 'cheap-source-map',
-  };
+
+        mode: isMode ? 'development' : 'production',
+
+        output: {
+            path: path.join(paths.build, 'js'),
+            filename: '[name].min.js',
+            publicPath: '/',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+
+                    resolve: {
+                        fullySpecified: false,
+                    },
+                },
+            ],
+        },
+        devtool: isMode ? 'source-map' : 'cheap-source-map',
+    };
 };
