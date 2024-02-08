73035f54a8f94eb19ec8203ef05d7d9351a9c5da
3p-vln <vldknovak@gmail.com>
2024-02-08 09:34:18 +0200

    fix

--- src/js/art.js
+++ src/js/art.js
@@ -3,12 +3,13 @@ import { db } from './modules/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import { artLoad } from './art/artLoad';
 import { focus } from './art/focus';
-import { preloader } from './modules/preloader';
-import { querySnapshot } from './modules/querySnapshot';
 
 document.addEventListener('DOMContentLoaded', async () => {
     async function snap() {
-        await querySnapshot('art', doc => new artLoad(doc.data(), doc.id, '.art-mesh'));
+        const querySnapshot = await getDocs(collection(db, 'art'));
+        querySnapshot.forEach(doc => {
+            new artLoad(doc.data(), doc.id, '.art-mesh');
+        });
     }
     snap();
     focus('.art-mesh');
