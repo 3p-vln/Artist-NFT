385e147302fa2fadd03066e4d189419dbb9df76a
3p-vln <vldknovak@gmail.com>
2024-02-14 12:31:42 +0200

    fix

--- src/js/modules/preloader.js
+++ src/js/modules/preloader.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
-export function preloader() {
-    // $('.preloader').delay(1000).fadeOut(500);
-    $(document).ready(function () {
-        $('.preloader').delay(1000).fadeOut(500);
+export async function preloader() {
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            const loader = document.querySelector('.preloader');
+            loader.classList.add('loaded');
+        }, 3000);
     });
 }
