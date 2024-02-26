5314a5e953bd5cf63b3ea478438edd80a90b6836
Your Name <you@example.com>
2024-02-26 16:52:01 +0200

    fix validate on auction and try to rewrite value

--- src/js/auction.js
+++ src/js/auction.js
@@ -1,9 +1,11 @@
+import { auctionToBD } from './auction/auctionToBd';
 import { snapAuction } from './auction/auctionLoad';
 import { auctionAccordion } from './auction/auctionAccardion';
 import { auctionValidate } from './auction/auctionValidate';
 import { auctionTimer } from './auction/auctionTimer';
 
 document.addEventListener('DOMContentLoaded', async () => {
+    await auctionToBD();
     await snapAuction();
     auctionTimer();
     auctionValidate();
