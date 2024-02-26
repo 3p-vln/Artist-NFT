9ac1e5b436cac235c174dbc4d3fd764658619f5a
Your Name <you@example.com>
2024-02-26 14:20:28 +0200

    before pull main

--- src/js/school.js
+++ src/js/school.js
@@ -1,26 +1,25 @@
-const buttons = document.querySelectorAll('.tabs__button');
-const cards = document.querySelectorAll('.card');
+    const buttons = document.querySelectorAll('.tabs__button');
+    const cards = document.querySelectorAll('.card');
 
- 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    let currentButton = button;
-    let tabId = currentButton.getAttribute('data-tab');
-    let currentCard = document.querySelector(tabId);
-    console.log(currentCard);
-    console.log('cliked', tabId)
-    
-    buttons.forEach((btn) => {
-      btn.classList.remove('tabs__button_active');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            let currentButton = button;
+            let tabId = currentButton.getAttribute('data-tab');
+            let currentCard = document.querySelector(tabId);
+            console.log(currentCard);
+            console.log('cliked', tabId);
+
+            buttons.forEach(btn => {
+                btn.classList.remove('tabs__button_active');
+            });
+
+            cards.forEach(card => {
+                card.classList.remove('card_active');
+            });
+
+            currentButton.classList.add('tabs__button_active');
+            currentCard.classList.add('card_active');
+        });
     });
 
-    cards.forEach((card) => {
-      card.classList.remove('card_active');
-    });
-
-    currentButton.classList.add('tabs__button_active');
-    currentCard.classList.add('card_active');
-  });
-});
-
-buttons[0].click();
+    buttons[0].click();
