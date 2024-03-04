02af2dfae04419ffb392edc2da3078038707ccd6
VUE Andrey Chaika <you@example.com>
2024-03-04 15:25:02 +0200

    fix swiper

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -25,6 +25,7 @@ export function premiumSwiper() {
   new Swiper('.premium-swiper', {
     loop: true,
     centeredSlides: true,
+    maxBackfaceHiddenSlides: 30,
 
     breakpoints: {
       320: {
