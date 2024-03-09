8593c07fdfba234c553b9524c9c67765299e21dc
VUE Andrey Chaika <you@example.com>
2024-03-09 16:32:11 +0200

    try fix swiper

--- src/js/main/premium.js
+++ src/js/main/premium.js
@@ -3,12 +3,12 @@ import { collection, getDocs } from 'firebase/firestore';
 import { getElementId } from '../composables/callDom.js';
 import { premiumLoad } from './premiumLoad';
 
-export async function snapPremiumSwiper() {
-    const querySnapshot = await getDocs(collection(db, 'main-swiper'));
-    querySnapshot.forEach((doc) => {
-        premiumLoad(doc.data(), doc.id);
-    });
-}
+// export async function snapPremiumSwiper() {
+//     const querySnapshot = await getDocs(collection(db, 'main-swiper'));
+//     querySnapshot.forEach((doc) => {
+//         premiumLoad(doc.data(), doc.id);
+//     });
+// }
 
 export async function snapPremiumBanner() {
   const querySnapshot = await getDocs(collection(db, 'main-premium'));
--- src/js/main/premiumLoad.js
+++ src/js/main/premiumLoad.js
@@ -1,27 +1,27 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-export function premiumLoad(data, id) {
-  const premium = $('.premium-wrapper');
+// export function premiumLoad(data, id) {
+//   const premium = $('.premium-wrapper');
 
-  premium.append(`
-                <div class="swiper-slide premium-slide ${id}">
-                    <h2 class="premium-slide__nft">${data.cardNftText}</h2>
-                    <div class="premium-slide__item">
-                    <picture>
-                        <source srcset="${data.cardImgWebp}" type="webp">
-                        <img src="${data.cardImg}" alt="item" class="premium-slide__item-img">
-                    </picture>    
-                    </div>
-                    <h3 class="premium-slide__title">${data.cardTitle}</h3>
-                    <p class="premium-slide__paragraph">${data.cardParagraph}</p>
-                    <div class="premium-slide__price">
-                        <h2 class="premium-slide__price-value">${data.cardValue}</h2>
-                        <a href="pay.html?id=${id}&name=nft">
-                            <button class="premium-slide__price-btn">
-                                <p>${data.cardBtnText}</p>
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                `);
-}
+//   premium.append(`
+//                 <div class="swiper-slide premium-slide ${id}">
+//                     <h2 class="premium-slide__nft">${data.cardNftText}</h2>
+//                     <div class="premium-slide__item">
+//                     <picture>
+//                         <source srcset="${data.cardImgWebp}" type="webp">
+//                         <img src="${data.cardImg}" alt="item" class="premium-slide__item-img">
+//                     </picture>    
+//                     </div>
+//                     <h3 class="premium-slide__title">${data.cardTitle}</h3>
+//                     <p class="premium-slide__paragraph">${data.cardParagraph}</p>
+//                     <div class="premium-slide__price">
+//                         <h2 class="premium-slide__price-value">${data.cardValue}</h2>
+//                         <a href="pay.html?id=${id}&name=nft">
+//                             <button class="premium-slide__price-btn">
+//                                 <p>${data.cardBtnText}</p>
+//                             </button>
+//                         </a>
+//                     </div>
+//                 </div>
+//                 `);
+// }
