a862e2c36b467f6745fcde3f251ce19098b27d14
VUE Andrey Chaika <you@example.com>
2024-03-10 14:36:04 +0200

    auction value fix

--- src/js/auction/auctionLoad.js
+++ src/js/auction/auctionLoad.js
@@ -3,16 +3,25 @@ import { collection, getDocs } from 'firebase/firestore';
 import { getElementId } from '../composables/callDom.js';
 
 export async function snapAuction() {
-  const querySnapshot = await getDocs(collection(db, 'auction'));
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    getElementId('imgWebp').src = data.imgWebp;
-    getElementId('imgPng').src = data.imgPng;
-    getElementId('title').innerHTML = data.title;
-    getElementId('size').innerHTML = data.size;
-    getElementId('estimatedValue').innerHTML = data.estimatedValue;
-    getElementId('highestPrice').innerHTML = `$${data.highestPrice}`;
-    getElementId('titleDescription').innerHTML = data.titleDescription;
-    getElementId('paragraphDescription').innerHTML = data.paragraphDescription;
-  });
+    const querySnapshot = await getDocs(collection(db, 'auction'));
+    querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        getElementId('imgWebp').src = data.imgWebp;
+        getElementId('imgPng').src = data.imgPng;
+        getElementId('title').innerHTML = data.title;
+        getElementId('size').innerHTML = data.size;
+        getElementId('estimatedValue').innerHTML = data.estimatedValue;
+        getElementId('highestPrice').innerHTML = `$${data.highestPrice}`;
+        getElementId('titleDescription').innerHTML = data.titleDescription;
+        getElementId('paragraphDescription').innerHTML = data.paragraphDescription;
+    });
+
+    let priceElement = getElementId('highestPrice');
+    let priceValue = priceElement.textContent;
+
+    if (priceValue.length > 7) {
+        let truncatedValue = priceValue.slice(0, 4) + '...' + priceValue.slice(-3);
+        priceElement.textContent = truncatedValue;
+        priceElement.title = priceValue;
+    }
 }
