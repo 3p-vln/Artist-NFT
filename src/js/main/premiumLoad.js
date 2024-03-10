faf62fe390fce27afd6ca56e9ddf613c13d49d23
VUE Andrey Chaika <you@example.com>
2024-03-10 15:50:58 +0200

    fix buttons

--- src/js/main/premiumLoad.js
+++ src/js/main/premiumLoad.js
@@ -1,14 +1,12 @@
-import $ from 'jquery'; 
- 
-export function premiumLoad(slide) { 
-    const premium = $('.premium__slider'); 
-    premium.html('');  
- 
-    let swiperSlide = ''; 
- 
-    slide.forEach((item) => { 
-        const id = item.id; 
-        console.log(id); 
+import $ from 'jquery';
+
+export function premiumLoad(slide) {
+    const premium = $('.premium__slider');
+    premium.html('');
+
+    let swiperSlide = '';
+
+    slide.forEach((item) => {
         const cardSlide = ` 
   <div class="swiper-slide premium-slide"> 
     <h2 class="premium-slide__nft">${item.cardNftText}</h2> 
@@ -22,23 +20,21 @@ export function premiumLoad(slide) {
     <p class="premium-slide__paragraph">${item.cardParagraph}</p> 
     <div class="premium-slide__price"> 
         <h2 class="premium-slide__price-value">${item.cardValue}</h2> 
-        <a href="pay.html?id=${item.id}&name=nft"> 
-            <button class="premium-slide__price-btn"> 
-                <p>${item.cardBtnText}</p> 
-            </button> 
+        <a href="pay.html?id=${item.id}&name=nft" class="premium-slide__price-btn"> 
+            <p>${item.cardBtnText}</p> 
         </a> 
     </div> 
-</div>`; 
- 
-        swiperSlide += cardSlide;  
-    }); 
- 
+</div>`;
+
+        swiperSlide += cardSlide;
+    });
+
     const swiperWrapper = ` 
     <div class="swiper premium-swiper"> 
         <div class="swiper-wrapper premium-wrapper"> 
             ${swiperSlide} 
         </div> 
-    </div>`; 
- 
-    premium.append(swiperWrapper);  
+    </div>`;
+
+    premium.append(swiperWrapper);
 }
