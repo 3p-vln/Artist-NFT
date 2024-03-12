69ee8afb5b2b8c839d06923c8fce297c0f03d31e
VUE Andrey Chaika <you@example.com>
2024-03-12 16:02:08 +0200

    new swiper

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,7 +3,7 @@ import 'slick-carousel';
 import Swiper from 'swiper';
 
 export function biographySwiper() {
-    let swiper = new Swiper('.columns-swiper', {
+    new Swiper('.columns-swiper', {
         slidesPerView: 'auto',
         breakpoints: {
             576: {
@@ -26,85 +26,16 @@ export function premiumSwiper() {
         arrows: false,
         slidesToShow: 5,
         variableWidth: true,
-        // slidesToScroll: "auto",
-        // responsive: [
-        //     {
-        //         breakpoints: 1700,
-        //         settings: {
-        //             slidesToShow: 5,
-        //         },
-        //     },
-        //     {
-        //         breakpoints: 1439,
-        //         settings: {
-        //             slidesToShow: 4,
-        //         },
-        //     },
-        //     {
-        //         breakpoints: 1200,
-        //         settings: {
-        //             slidesToShow: 3,
-        //         },
-        //     },
-        //     {
-        //         breakpoints: 768,
-        //         settings: {
-        //             slidesToShow: 3,
-        //         },
-        //     },
-        //     {
-        //         breakpoints: 430,
-        //         settings: {
-        //             slidesToShow: 2,
-        //         },
-        //     },
-        //     {
-        //         breakpoints: 320,
-        //         settings: {
-        //             slidesToShow: 1,
-        //         },
-        //     },
-        // ],
+        responsive: [
+            {
+                breakpoints: 576,
+                settings: {
+                    centerMode: false,
+                },
+            },
+        ],
     });
 }
-
-// export function premiumSwiper() {
-//     new Swiper('.premium-swiper', {
-//         loop: true,
-//         centeredSlides: true,
-
-//         breakpoints: {
-//             320: {
-//                 slidesPerView: 1.3,
-//                 spaceBetween: 16,
-//             },
-//             430: {
-//                 slidesPerView: 1.7,
-//                 spaceBetween: 16,
-//             },
-//             768: {
-//                 slidesPerView: 2.5,
-//                 spaceBetween: 30,
-//             },
-//             1200: {
-//                 slidesPerView: 3,
-//                 spaceBetween: 30,
-//             },
-//             1439: {
-//                 slidesPerView: 3.8,
-//                 spaceBetween: 30,
-//             },
-//             1700: {
-//                 slidesPerView: 5,
-//                 spaceBetween: 30,
-//             },
-//             1920: {
-//                 slidesPerView: 5,
-//                 spaceBetween: 30,
-//             },
-//         },
-//     });
-// }
 
 export function popularSwiper() {
     new Swiper('.popular-swiper', {
