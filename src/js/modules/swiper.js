85494ec4ca8395d72037932ab25987d4c7c9878d
VUE Andrey Chaika <you@example.com>
2024-03-12 14:49:16 +0200

    fix swiper slick

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,6 +1,6 @@
-// import Swiper, { Navigation, Pagination, Autoplay, Scrollbar } from 'swiper';
+import $ from 'jquery';
+import 'slick-carousel';
 import Swiper from 'swiper';
-
 
 export function biographySwiper() {
     let swiper = new Swiper('.columns-swiper', {
@@ -21,42 +21,90 @@ export function biographySwiper() {
 }
 
 export function premiumSwiper() {
-    new Swiper('.premium-swiper', {
-        loop: true,
-        centeredSlides: true,
-
-        breakpoints: {
-            320: {
-                slidesPerView: 1.3,
-                spaceBetween: 16,
-            },
-            430: {
-                slidesPerView: 1.7,
-                spaceBetween: 16,
-            },
-            768: {
-                slidesPerView: 2.5,
-                spaceBetween: 30,
-            },
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-            1439: {
-                slidesPerView: 3.8,
-                spaceBetween: 30,
-            },
-            1700: {
-                slidesPerView: 5,
-                spaceBetween: 30,
-            },
-            1920: {
-                slidesPerView: 5,
-                spaceBetween: 30,
-            },
-        },
+    $('.premium-wrapper').slick({
+        centerMode: true,
+        arrows: false,
+        slidesToShow: 5,
+        variableWidth: true,
+        // slidesToScroll: "auto",
+        // responsive: [
+        //     {
+        //         breakpoints: 1700,
+        //         settings: {
+        //             slidesToShow: 5,
+        //         },
+        //     },
+        //     {
+        //         breakpoints: 1439,
+        //         settings: {
+        //             slidesToShow: 4,
+        //         },
+        //     },
+        //     {
+        //         breakpoints: 1200,
+        //         settings: {
+        //             slidesToShow: 3,
+        //         },
+        //     },
+        //     {
+        //         breakpoints: 768,
+        //         settings: {
+        //             slidesToShow: 3,
+        //         },
+        //     },
+        //     {
+        //         breakpoints: 430,
+        //         settings: {
+        //             slidesToShow: 2,
+        //         },
+        //     },
+        //     {
+        //         breakpoints: 320,
+        //         settings: {
+        //             slidesToShow: 1,
+        //         },
+        //     },
+        // ],
     });
 }
+
+// export function premiumSwiper() {
+//     new Swiper('.premium-swiper', {
+//         loop: true,
+//         centeredSlides: true,
+
+//         breakpoints: {
+//             320: {
+//                 slidesPerView: 1.3,
+//                 spaceBetween: 16,
+//             },
+//             430: {
+//                 slidesPerView: 1.7,
+//                 spaceBetween: 16,
+//             },
+//             768: {
+//                 slidesPerView: 2.5,
+//                 spaceBetween: 30,
+//             },
+//             1200: {
+//                 slidesPerView: 3,
+//                 spaceBetween: 30,
+//             },
+//             1439: {
+//                 slidesPerView: 3.8,
+//                 spaceBetween: 30,
+//             },
+//             1700: {
+//                 slidesPerView: 5,
+//                 spaceBetween: 30,
+//             },
+//             1920: {
+//                 slidesPerView: 5,
+//                 spaceBetween: 30,
+//             },
+//         },
+//     });
+// }
 
 export function popularSwiper() {
     new Swiper('.popular-swiper', {
