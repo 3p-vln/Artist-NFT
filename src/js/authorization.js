8745491ab93418f45fd6cd3f4e3ebaf243397aec
3p-vln <vldknovak@gmail.com>
2024-03-21 09:29:37 +0200

    fix forum

--- src/js/authorization.js
+++ src/js/authorization.js
@@ -2,17 +2,15 @@ import JustValidate from 'just-validate';
 import { tab } from './pay/tabs';
 import { radio } from './authorization/radio';
 import { stop } from './modules/stopPreload';
-// import { validate } from './authorization/authValidate';
+import { validate } from './authorization/authValidate';
 // import { signInUp } from './authorization/signin';
 import { checkAuthLinks } from './authorization/chekAuthLinks';
 
 document.addEventListener('DOMContentLoaded', async () => {
     tab();
     radio();
-    // validate();
+    validate();
     // signInUp();
-  checkAuthLinks();
+    checkAuthLinks();
     stop();
 });
-
-
